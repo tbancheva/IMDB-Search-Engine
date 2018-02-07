325217625c6d252b1e893a2bd7ec6be1b9c578c9
tbancheva <teodorabancheva@abv.bg>
2018-02-07 14:34:31 +0200

    Update Main.java

--- src/bg/uni/sofia/fmi/mjt/imdb/Main.java
+++ src/bg/uni/sofia/fmi/mjt/imdb/Main.java
@@ -1,7 +1,10 @@
 package bg.uni.sofia.fmi.mjt.imdb;
 
+
 import java.io.IOException;
+
 import java.util.Scanner;
+
 
 public class Main {
 
@@ -11,29 +14,32 @@ public class Main {
 		Thread server = new Thread(new IMDbServer(localMDb));
 		Scanner input = new Scanner(System.in);
 		
-		Thread client1 = new Thread(new IMDbClient(input));
-		//Thread client2 = new Thread(new IMDbClient(input));
-		//Thread client3 = new Thread(new IMDbClient(input));
-		//Thread client4 = new Thread(new IMDbClient(input));
-		client1.start();
-		//client2.start();
-		//client3.start();
-		//client4.start();
+		System.out.println("Enter number of clients: ");
+		int numberOfClients = input.nextInt();
+		input.nextLine(); // Consuming new-line left-over
+		
+		Thread[] threads = new Thread[numberOfClients];
+		
+		for(int i=0; i<numberOfClients;i++) {
+			threads[i] = new Thread(new IMDbClient(input));
+			threads[i].start();
+		}
+				
 		server.start();
-	
-		
-		//try {
-		//	client1.join();
-		//	client2.join();
-			//client3.join();
-		//} catch (InterruptedException e) {
-		//	e.printStackTrace();
-		//}
-					
 			
-		input.close();
-	
+		try {
+			for(int i=0; i<numberOfClients;i++) {
+				threads[i].join();
+			}
 		
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+				
+		input.close();
+		
+				
 	}
+
 	
 }
